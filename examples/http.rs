3fdd129077f514af5ef9890c0f17e8a3e4163ad8
Colin Kirkpatrick <kibbles@kibblesnbits.server.world>
2016-12-18 14:13:08 -0500

    Going to use asynch setup instead of old fashioned http

--- examples/http.rs
+++ examples/http.rs
@@ -2,7 +2,7 @@ extern crate futures;
 extern crate tokio_core as tokio;
 extern crate tokio_service;
 extern crate tokio_proto;
-extern crate tiny_http;
+extern crate tokio_minihttp;
 //extern crate protocol_manager;
 extern crate maud;
 
@@ -12,20 +12,10 @@ extern crate maud;
 //tokio-proto= { git = "https://github.com/tokio-rs/tokio-proto" }
 //tiny-http = "0.5"
 
-use tiny_http::{ Server, Response };
+use tokio_minihttp::{ Server, Response };
 use tokio::reactor;
 
 fn main() {
-    let server = tiny_http::Server::http("0.0.0.0:3000").unwrap();
 
-    for request in server.incoming_requests() {
-        println!("Request:\nMethod: {:?}\nURL: {:?}\nHeaders: {:?}",
-            request.method(),
-            request.url(),
-            request.headers());
-
-        let response = Response::from_string("Hello : dude");
-        request.respond(response);
-    }
 }
 
