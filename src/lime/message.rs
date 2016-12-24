44adc9e80b61acf8bec7d24f8980b504e96b355c
kibbles <colindjk@gmail.com>
2016-12-24 15:12:25 -0500

    First use of macro, going to flush out macro stuff

--- src/lime/message.rs
+++ src/lime/message.rs
@@ -1,13 +1,18 @@
 use serde_json::{ Map, Value };
+use lime::envelope::*;
 
 pub struct Message { map: Map<String, Value> }
 
+impl_Envelope!(Message,
+               MessageType,
+               |_| Some(MessageType::Normal),
+               Some(MessageType::Normal));
+
 /// TODO: Figure out other possible message types.
 pub enum MessageType {
+    Normal,
     Chat,
     Groupchat,
     Error
 }
-
-impl_Envelope!(Message, );
 
