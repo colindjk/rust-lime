4ace4b1221d76f8688389bbaefeb956cc3b1233a
kibbles <colindjk@gmail.com>
2016-12-28 10:14:36 -0500

    Serialization completed

---
+++ src/envelope/codec.rs
@@ -0,0 +1,37 @@
+//use tokio_core::io::{
+    //Codec,
+    //EasyBuf
+//};
+
+//use std::io;
+//use std::str;
+//use lime::{ SealedEnvelope, DELIMITER };
+//use serde_json;
+//use serde_json::value::*;
+
+pub struct LimeCodec;
+
+//impl Codec for LimeCodec {
+    //type In = SealedEnvelope;
+    //type Out = SealedEnvelope;
+
+    //fn decode(&mut self, buf: &mut EasyBuf)
+              //-> Result<Option<Self::In>, io::Error> {
+        //match buf.as_slice().iter().position(|&b| b == DELIMITER) {
+            //Some(index) => {
+                //println!("Decoding {}", index);
+                //let object_buf = buf.drain_to(index + 1);
+                //// TODO: Clean the below up so it doesn't blow up
+                //Ok(Some(serde_json::from_slice(object_buf.as_slice()).unwrap()))
+            //}
+            //None => Ok(None)
+        //}
+    //}
+
+    //// TODO: Make a Envelope based method which produces a Vec<u8>, or
+    //// implement serialize for SealedEnvelope.
+    //fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        //panic!()
+    //}
+//}
+
---
+++ src/envelope/message.rs
@@ -0,0 +1,37 @@
+use serde_json::{ Value };
+
+use lime::envelope::*;
+use lime::JsonMap;
+
+pub type Content = Value;
+
+#[derive(Debug, Serialize)]
+pub struct Message {
+    pub to: Option<Node>,
+    pub from: Option<Node>,
+    pub pp: Option<Node>,
+    pub id: Option<MsgID>,
+    pub metadata: Option<JsonMap>,
+
+    pub mime_type: String,
+    pub content: Content,
+}
+
+impl Message {
+
+}
+
+impl_Envelope!(Message,
+               MessageType,
+               |_| Some(MessageType::Normal),
+               Some(MessageType::Normal),
+               "content");
+
+/// TODO: Figure out other possible message types.
+pub enum MessageType {
+    Normal,
+    Chat,
+    Groupchat,
+    Error
+}
+
