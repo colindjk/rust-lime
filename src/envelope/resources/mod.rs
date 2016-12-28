8d672e4eb90ba8bfc6d9eacfa9fdcffbbffda772
kibbles <colindjk@gmail.com>
2016-12-28 14:46:08 -0500

    Moved resources into envelope module

---
+++ src/envelope/resources/mod.rs
@@ -0,0 +1,28 @@
+
+pub enum Resource {
+    Account(Account),
+}
+
+/// Represents the user information as a series of options.
+#[derive(Debug)]
+pub struct Account {
+    full_name: Option<String>,
+    address: Option<String>,
+    city: Option<String>,
+    email: Option<String>,
+    phone_number: Option<String>,
+    photo_uri: Option<String>,
+    cell_phone_number: Option<String>,
+    is_temporary: Option<bool>,
+    password: Option<String>,
+    old_password: Option<String>,
+    inbox_size: Option<String>,
+    allow_guest_sender: Option<bool>,
+    allow_unknown_sender: Option<bool>,
+    store_message_content: Option<bool>,
+}
+
+pub struct Capability {
+
+}
+
