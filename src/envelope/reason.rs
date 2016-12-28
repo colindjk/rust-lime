ae6f04beacea1dc9e26e91625ecdfd8dd70ab6bd
kibbles <colindjk@gmail.com>
2016-12-28 11:35:15 -0500

    Included ReasonCode, TDOO: Refactor ErrReason

---
+++ src/envelope/reason.rs
@@ -0,0 +1,52 @@
+macro_rules! enum_number {
+    ($name:ident { $($variant:ident = $value:expr, )* }) => {
+        #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+        pub enum $name {
+            $($variant = $value,)*
+        }
+
+        impl ::serde::Serialize for $name {
+            fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
+                where S: ::serde::Serializer,
+            {
+                // Serialize the enum as a u64.
+                serializer.serialize_u64(*self as u64)
+            }
+        }
+
+        impl ::serde::Deserialize for $name {
+            fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
+                where D: ::serde::Deserializer,
+            {
+                struct Visitor;
+
+                impl ::serde::de::Visitor for Visitor {
+                    type Value = $name;
+
+                    fn visit_u64<E>(&mut self, value: u64) -> Result<$name, E>
+                        where E: ::serde::de::Error,
+                    {
+                        // Rust does not come with a simple way of converting a
+                        // number to an enum, so use a big `match`.
+                        match value {
+                            $( $value => Ok($name::$variant), )*
+                            _ => Err(E::invalid_value(
+                                &format!("unknown {} value: {}",
+                                stringify!($name), value))),
+                        }
+                    }
+                }
+
+                // Deserialize the enum from a u64.
+                deserializer.deserialize_u64(Visitor)
+            }
+        }
+    }
+}
+
+// TODO : Complete this
+enum_number!(ReasonCode {
+    GENERAL_ERROR = 0,
+    SESSION_REGISTRATION_ERROR = 12,
+    SESSION_AUTHENTICATION_FAILED = 13,
+});
