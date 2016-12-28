91c0f17fdc606e2c9d2d85343e7be0bf55898069
kibbles <colindjk@gmail.com>
2016-12-28 11:49:25 -0500

    Macro refactor

--- src/envelope/mod.rs
+++ src/envelope/mod.rs
@@ -4,22 +4,33 @@ use serde_json::{ Map, Value };
 pub static DELIMITER : u8 = b'\n' as u8;
 type JsonMap = Map<String, Value>;
 
+// Envelope types
+#[macro_use]
+pub mod macros;
+
 pub mod message;
 pub mod notification;
 pub mod command;
 pub mod session;
+
+// Types heald by envelopes
+pub mod reason;
 
 mod ser;
 mod codec;
 mod helper;
 
 pub use self::codec::LimeCodec;
+
 pub use self::message::{Message, Content};
 pub use self::notification::{Notification, NotificationEvent};
 pub use self::command::Command;
 pub use self::session::*;
 
+pub use self::reason::Reason as ErrReason;
+
 pub type Node = String;
+pub type Resources = Value;
 pub type MsgID = String;
 pub type TimeStamp = u64;
 
@@ -33,14 +44,6 @@ pub enum SealedEnvelope {
     SessionReq(SessionRequest),
     SessionRes(SessionResponse),
     Unknown(JsonMap),
-}
-
-/// When an Error occurs, this will exist.
-/// TODO: Use this for other structs aside from just Notification.
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
-pub struct ErrReason {
-    pub code: u8,
-    pub description: Option<String>
 }
 
 /// Trait for all envelope related types.
@@ -63,23 +66,3 @@ pub trait Envelope {
     //fn set_pp(&self) -> Option<Node>;
     //fn set_metadata(&self) -> Option<Node>;
 }
-
-macro_rules! impl_Envelope(
-    ($kind: ident, $ty: ty, $ty_some: expr, $ty_none: expr, $unique_field: expr) => (
-        impl Envelope for $kind {
-            //type Ty = $ty;
-
-            //fn unique<'a>() -> &'a str { $unique_field }
-
-            fn id(&self) -> Option<&str> { self.id.as_ref().map(|s| &**s) }
-            fn to(&self) -> Option<&str> { self.to.as_ref().map(|s| &**s) }
-            fn from(&self) -> Option<&str> { self.from.as_ref().map(|s| &**s) }
-            fn set_id(&mut self, id: Option<String>) { self.id = id; }
-            fn set_to(&mut self, to: Option<String>) { self.to = to; }
-            fn set_from(&mut self, from: Option<String>) { self.from = from; }
-
-        }
-
-    );
-);
-
--- src/envelope/reason.rs
+++ src/envelope/reason.rs
@@ -1,52 +1,17 @@
-macro_rules! enum_number {
-    ($name:ident { $($variant:ident = $value:expr, )* }) => {
-        #[derive(Clone, Copy, Debug, Eq, PartialEq)]
-        pub enum $name {
-            $($variant = $value,)*
-        }
 
-        impl ::serde::Serialize for $name {
-            fn serialize<S>(&self, serializer: &mut S) -> Result<(), S::Error>
-                where S: ::serde::Serializer,
-            {
-                // Serialize the enum as a u64.
-                serializer.serialize_u64(*self as u64)
-            }
-        }
-
-        impl ::serde::Deserialize for $name {
-            fn deserialize<D>(deserializer: &mut D) -> Result<Self, D::Error>
-                where D: ::serde::Deserializer,
-            {
-                struct Visitor;
-
-                impl ::serde::de::Visitor for Visitor {
-                    type Value = $name;
-
-                    fn visit_u64<E>(&mut self, value: u64) -> Result<$name, E>
-                        where E: ::serde::de::Error,
-                    {
-                        // Rust does not come with a simple way of converting a
-                        // number to an enum, so use a big `match`.
-                        match value {
-                            $( $value => Ok($name::$variant), )*
-                            _ => Err(E::invalid_value(
-                                &format!("unknown {} value: {}",
-                                stringify!($name), value))),
-                        }
-                    }
-                }
-
-                // Deserialize the enum from a u64.
-                deserializer.deserialize_u64(Visitor)
-            }
-        }
-    }
+/// When an Error occurs, this will exist.
+/// TODO: Use this for other structs aside from just Notification.
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Reason {
+    pub code: ReasonCode,
+    pub description: Option<String>
 }
 
 // TODO : Complete this
-enum_number!(ReasonCode {
-    GENERAL_ERROR = 0,
-    SESSION_REGISTRATION_ERROR = 12,
-    SESSION_AUTHENTICATION_FAILED = 13,
+enum_number!(
+ReasonCode {
+    GeneralError = 0,
+    SessionRegistrationError = 12,
+    SessionAuthenticationFailed = 13,
 });
+
