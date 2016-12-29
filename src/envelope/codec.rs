1e1e2ff119d6e7c578700381c9d6c40d193bf9ff
kibbles <colindjk@gmail.com>
2016-12-28 21:12:44 -0500

    Reimplemented LimeCodec to outline a line-delimited json parser

--- src/envelope/codec.rs
+++ src/envelope/codec.rs
@@ -1,37 +1,45 @@
-//use tokio_core::io::{
-    //Codec,
-    //EasyBuf
-//};
+use tokio_core::io::{
+    Codec,
+    EasyBuf
+};
 
-//use std::io;
-//use std::str;
-//use lime::{ SealedEnvelope, DELIMITER };
-//use serde_json;
-//use serde_json::value::*;
+use std::io;
+use std::str;
+use envelope::{ SealedEnvelope, DELIMITER };
 
 pub struct LimeCodec;
 
-//impl Codec for LimeCodec {
-    //type In = SealedEnvelope;
-    //type Out = SealedEnvelope;
+impl Codec for LimeCodec {
+    type In = SealedEnvelope;
+    type Out = SealedEnvelope;
 
-    //fn decode(&mut self, buf: &mut EasyBuf)
-              //-> Result<Option<Self::In>, io::Error> {
-        //match buf.as_slice().iter().position(|&b| b == DELIMITER) {
-            //Some(index) => {
-                //println!("Decoding {}", index);
-                //let object_buf = buf.drain_to(index + 1);
-                //// TODO: Clean the below up so it doesn't blow up
-                //Ok(Some(serde_json::from_slice(object_buf.as_slice()).unwrap()))
-            //}
-            //None => Ok(None)
-        //}
-    //}
+    fn decode(&mut self, buf: &mut EasyBuf)
+              -> Result<Option<Self::In>, io::Error> {
+        match buf.as_slice().iter().position(|&b| b == DELIMITER) {
+            Some(index) => {
+                let buf = buf.drain_to(index + 1);
+                use serde_json::from_slice;
+                if let Ok(envelope) = from_slice(buf.as_slice()) {
+                    Ok(Some(envelope))
+                } else {
+                    unimplemented!()
+                }
+            }
+            None => Ok(None)
+        }
+    }
 
-    //// TODO: Make a Envelope based method which produces a Vec<u8>, or
-    //// implement serialize for SealedEnvelope.
-    //fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
-        //panic!()
-    //}
-//}
+    fn encode(&mut self, msg: Self::Out, buf: &mut Vec<u8>) -> io::Result<()> {
+        use serde_json::ser::to_vec;
+        if let Ok(mut json) = to_vec(&msg) {
+            buf.append(&mut json);
+            buf.push(DELIMITER.clone());
+            Ok(())
+        } else {
+            Err(io::Error::new(
+                io::ErrorKind::Other, "Failed to encode object".to_string()))
+        }
+    }
+
+}
 
