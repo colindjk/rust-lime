5ea623d3a80717b9ff2a023f29c180d52fc5f684
kibbles <colindjk@gmail.com>
2016-12-29 08:07:13 -0500

    Not ready for impl

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,11 +1,12 @@
-use std::str;
-use std::io::{self, ErrorKind, Write};
+//use std::str;
+//use std::io::{self, ErrorKind, Write};
 
-use futures::{future, Future, BoxFuture};
-use tokio_core::io::{Io, Codec, Framed, EasyBuf};
-use tokio_proto::TcpServer;
-use tokio_proto::pipeline::ServerProto;
-use tokio_service::Service;
+//use futures::{future, Future, BoxFuture};
+//use tokio_core::io::{Io, Codec, Framed, EasyBuf};
+//use tokio_proto::TcpServer;
+//use tokio_proto::pipeline::ServerProto;
+//use tokio_service::Service;
 
+//use envelope::LimeCodec;
 
 
