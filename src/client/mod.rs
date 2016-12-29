b849f9f3b52f83a19449109623fb943749836576
kibbles <colindjk@gmail.com>
2016-12-28 19:11:25 -0500

    Setting up client connection service which will take care of client connections

---
+++ src/client/mod.rs
@@ -0,0 +1,11 @@
+use std::str;
+use std::io::{self, ErrorKind, Write};
+
+use futures::{future, Future, BoxFuture};
+use tokio_core::io::{Io, Codec, Framed, EasyBuf};
+use tokio_proto::TcpServer;
+use tokio_proto::pipeline::ServerProto;
+use tokio_service::Service;
+
+
+
