b349c76764470b0f7c5877e66057c07a9931a15f
kibbles <colindjk@gmail.com>
2017-01-02 15:01:18 -0500

    Setting up the server types

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,41 +1,64 @@
 use std::io;
 
-use futures::{future, Future, BoxFuture};
-use tokio_core::io::{Io, Framed};
-use tokio_service::Service;
-use tokio_proto::multiplex::ServerProto;
+use std::net::SocketAddr;
+use std::io::{Error};
+use futures::{stream, Stream, future, Future, Poll, BoxFuture};
+use tokio_core::net::{TcpListener};
 
-use envelope::{LimeCodec, SealedEnvelope as Envelope};
+//use tokio_core::io::{Io, Framed};
+//use envelope::{LimeCodec, SealedEnvelope as Envelope};
 
-pub struct LimeProtocol;
+/// Initial server which can used to accept and work with connections.
+/// Generally it will be used to accept incoming connections.
+pub struct LimeServer {
+    addr: SocketAddr,
+    listener: TcpListener,
+};
 
-/// TODO: Make sure the Request gets to the right place (user / db?), then start
-/// to worry about the actual response (what the future returns).
+/// Implementation 
 
-/// This, uh... 'hooks up the codec'. Yeah.
-impl<T: Io + 'static> ServerProto<T> for LimeProtocol {
-    type Request = Envelope;
-    type Response = Envelope;
-    type Error = io::Error;
-    type Transport = Framed<T, LimeCodec>;
-    type BindTransport = io::Result<Framed<T, LimeCodec>>;
+pub struct Nodes {
+    inner: Server,
+}
 
-    fn bind_transport(&self, io: T) -> io::Result<Framed<T, LimeCodec>> {
-        Ok(io.framed(LimeCodec))
+/// A stream of incoming connections, which are evaluated to be Nodes.
+impl Stream for Nodes {
+    type Item = Node;
+    type Error = Error;
+
+    /// Attempt to make a connection to a Node.
+    fn poll(&mut self) -> Poll<Option<Self::Item>, Self::Error> {
+
     }
 }
 
-pub struct Server;
+//pub struct LimeProtocol;
 
-impl Service for Server {
-    type Request = Envelope;
-    type Response = Envelope;
-    type Error = io::Error;
-    type Future = BoxFuture<Envelope, io::Error>;
+///// TODO: Make sure the Request gets to the right place (user / db?), then start
+///// to worry about the actual response (what the future returns).
 
-    /// For now we'll just return the message that was sent
-    fn call(&self, req: Envelope) -> Self::Future {
-        future::finished(req).boxed()
-    }
-}
+///// This, uh... 'hooks up the codec'. Yeah.
+//impl<T: Io + 'static> ServerProto<T> for LimeProtocol {
+    //type Request = Envelope;
+    //type Response = Envelope;
+    //type Error = io::Error;
+    //type Transport = Framed<T, LimeCodec>;
+    //type BindTransport = io::Result<Framed<T, LimeCodec>>;
+
+    //fn bind_transport(&self, io: T) -> io::Result<Framed<T, LimeCodec>> {
+        //Ok(io.framed(LimeCodec))
+    //}
+//}
+
+//impl Service for Server {
+    //type Request = Envelope;
+    //type Response = Envelope;
+    //type Error = io::Error;
+    //type Future = BoxFuture<Envelope, io::Error>;
+
+    ///// For now we'll just return the message that was sent
+    //fn call(&self, req: Envelope) -> Self::Future {
+        //future::finished(req).boxed()
+    //}
+//}
 
