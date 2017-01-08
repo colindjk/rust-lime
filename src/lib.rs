9fb315e8dcf34297ee38306f9fa5d7de97c7fc23
kibbles <colindjk@gmail.com>
2017-01-08 11:37:25 -0500

    impl Trait usage :D

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![feature(proc_macro, custom_attribute, custom_derive, plugin)]
+#![feature(conservative_impl_trait)]
 #![plugin(serde_derive)]
 
 extern crate futures;
@@ -14,8 +15,9 @@ extern crate serde_urlencoded;
 extern crate serde_derive;
 
 pub mod net;
+pub mod user;
 //pub mod client; // temporary, the client will be back!
 #[macro_use]
-pub mod envelope;
+pub mod envelope; // protocol src
 pub mod utils;
 
