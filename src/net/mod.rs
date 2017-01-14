6e7801d54fe8f0edc6531495937d0790d67d3279
kibbles <colindjk@gmail.com>
2017-01-14 17:22:16 -0500

    Changed server to be abstract over the type of stream instead of type of server

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -4,7 +4,7 @@ use std::net::SocketAddr;
 use std::convert::{From};
 use std::io;
 use std::collections::HashMap;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 use futures::{stream, Stream, Sink, future, Future, Poll, BoxFuture};
 
@@ -18,15 +18,16 @@ use envelope::{Node, LimeCodec, EnvelopeStream, SealedEnvelope as Envelope};
 // TODO : Refactor to make sense
 pub use self::node::*;
 
+// TODO: Put a Mutex around that ClientSink!
 type NodeMap<S> = HashMap<Node, ClientSink<S>>;
+type ArcMut<T> = Arc<Mutex<T>>;
 
 /// Generally it will be used to accept incoming connections.
 /// 'L' will be any type of listener, which produces a stream of
 /// ClientConnection structs.
 ///
 /// TODO: Figure out a way to handle online users, is a HashMap optimal?
-pub struct LimeServer<S>
-{
+pub struct LimeServer<S> {
     addr: SocketAddr,
     users: Arc<NodeMap<S>>,
 }
@@ -41,6 +42,7 @@ impl<S> LimeServer<S>
     pub fn new(addr: &SocketAddr) -> Self {
         LimeServer {
             addr: addr.clone(),
+            users: Arc::new(HashMap::new())
         }
     }
 }
